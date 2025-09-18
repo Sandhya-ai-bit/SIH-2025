146e9de472114ac3bac0137a753342da90db3927
Sandhya-ai-bit <shawsandhya466@gmail.com>
2025-09-18 22:05:48 +0530

    added an sentence

--- frontend/pages/About.jsx
+++ frontend/pages/About.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 function About() {
   return (
     <>
-        <h1>About Us</h1>
+        <h1>About Us  </h1>
         {/* This part is assigned for Sandhya */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse quas ea deleniti, tempore consequatur delectus minus magnam recusandae provident, iste deserunt veritatis repellat nobis iusto cumque, praesentium aliquid odit?
+
+        </p>
     </>
   )
 }
